35b26eb0e4b1176d2454031accffe10841142ae1
Cliff Gor <clierozigy@gmail.com>
2019-05-14 00:12:38 +0300

    minor fix

---
+++ cred.js
@@ -0,0 +1,14 @@
+module.exports = {
+    AT:{
+        apiKey: "ff20182072a067e304330e0079ff4102df0dee3b6d56d6f53538a46a9aee6cdb",
+        username: "cliffgor",
+        format: 'json'
+    },
+    pusher:{
+       appId: '781767',
+       key: "11514cfa2ae115901ce5",
+       secret: "3e70ea23e9effb5978b3",
+       cluster: "mt1",
+       encrypted: true
+    }
+}
